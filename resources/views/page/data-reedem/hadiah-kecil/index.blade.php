238507b9d2e95a2046e960b41bf31866b0ac3aba
irfan <muhamadirfanm30@gmail.com>
2023-09-07 21:43:48 +0700

    update

--- resources/views/page/data-reedem/hadiah-kecil/index.blade.php
+++ resources/views/page/data-reedem/hadiah-kecil/index.blade.php
@@ -78,7 +78,8 @@
         </table>
         <div class="row">
             <!-- <div class="col-md-3">Showing <strong>1</strong> to <strong>{{ $last > '20' ? '20' : count($response)  }}</strong> of <strong>{{ $total_record }}</strong> entries</div> -->
-            <div class="col-md-3">Showing <strong>{{ 1 + (($page - 1)  * 20) }}</strong> to <strong>{{ $last > '20' ? 20 + (($page - 1)  * 20) : count($response)  }}</strong> of <strong>{{ $total_record }}</strong> entries</div>
+            <!-- <div class="col-md-3">Showing <strong>{{ 1 + (($page - 1)  * 20) }}</strong> to <strong>{{ $last > '20' ? 20 + (($page - 1)  * 20) : count($response)  }}</strong> of <strong>{{ $total_record }}</strong> entries</div> -->
+            <div class="col-md-3">Showing <strong>{{ 1 + (($page - 1)  * 20) }}</strong> to <strong>{{ $total_record > '20' ? 20 + (($page - 1)  * 20) : $total_record  }}</strong> of <strong>{{ $total_record }}</strong> entries</div>
             <div class="col-md-3"></div>
             <div class="col-md-2"></div>
             <div class="col-md-4 text-right">
